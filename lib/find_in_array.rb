d8c4ac611c61cff8de1599b963a0c3ac76c8274a
Andre Hochi <andrehochiwork@gmail.com>
2021-02-12 01:41:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -11,3 +11,16 @@ def find_element_index(array, value_to_find)
   found_value_index
 end
 
+
+
+def find_element_index(array, value_to_find)
+  count = 0
+  value = nil
+  while count < array.length do 
+    if array[count] == value_to_find do
+      value = count
+    end
+    count = count + 1
+  end
+  value
+end
